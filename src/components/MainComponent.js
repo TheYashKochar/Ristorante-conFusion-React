4c5c7e8d891a9f88398b8df7bc9f2ecde83c5c83
Yash Kochar <yash.kochar143@gmail.com>
2020-06-25 12:54:56 +0530

    React route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Component} from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 //function Main()
 class Main extends Component{
@@ -13,22 +15,24 @@ class Main extends Component{
     this.state = {
       // this dishes object will be passed child component "Menu"
       dishes: DISHES,
-      selectedDish: null
     };
   }
 
-  onDishSelect(dishID) {
-    //when user chooses a dish update state to "selectedDish to current dish"
-    this.setState({ selectedDish: dishID});
-    }
-
   render() {
+    //Another method to pass function component other than used in Menu part
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} 
-          onClick={(dishID) => this.onDishSelect(dishID)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />  
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
